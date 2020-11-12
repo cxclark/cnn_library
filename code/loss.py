3983777369016d9fe6b6422b9376389e1aa9d758
Caroline <caroline@humanperspectives.com>
2020-11-12 13:49:39 -0500

    updated loss.py

--- code/loss.py
+++ code/loss.py
@@ -1,16 +1,11 @@
 import numpy as np
 
-"""
-Layers that implement a loss.
-"""
-
 # Define a class to perform Softmax 
 class SoftmaxLayer:
     """
     This is a classifier, with N discrete classes from 0 to N-1.
-    
     The softmax function receives scores and interprets them as the 
-    unnormalized log probailities. It exponentiates and normalizes the
+    unnormalized log probabilities. It exponentiates and normalizes the
     scores to produce probabilties.
     """
     def __init__(self):
