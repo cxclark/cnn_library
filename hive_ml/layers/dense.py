59fc6e1a5382e07668654a996b3baa47e3d15726
Caroline <caroline@humanperspectives.com>
2020-11-17 23:45:37 -0500

    updated dense.py

--- hive_ml/layers/dense.py
+++ hive_ml/layers/dense.py
@@ -15,26 +15,33 @@ class DenseLayer:
         """
         Implements forward propagation of dense layer.
         Arguments:
-            X -- input data, of shape (64, 768)
+            X -- input data, numpy array of shape (flattened_neurons, batch_size).
+            W -- weights, numpy array of shape (num_classes, flattened_neurons). Pulled from self.params.
+            b -- biases, numpy array of shape (num_classes, 1). Pulled from self.params.
         Returns:
-            Z -- output scores to be passed to an activation function.
+            Z -- output scores to be passed to an activation function, numpy array of shape (num_classes, batch_size).
         """
 
         # Initialize a parameter matrix if it does not exist. 
         if 'W' not in self.params:
             self.params['W'], self.params['b'] = utils.he_normal((X.shape[0], self.units))
-            
+
+        # Extract W and b values and save to variables.
         W = self.params['W']
         b = self.params['b']
 
         # Save the input in the cache for backpropagation.
         self.cache['A'] = X
         
+        # Compute the dot product and add the bias.
         Z = np.dot(W, X) + b
 
         return Z
 
     def backward(self, dZ, lr):
+        """
+        Implements backward propagation of dense layer.
+        """
         batch_size = dZ.shape[1]
         self.cache['dW'] = np.dot(dZ, self.cache['A'].T) / batch_size
         self.cache['db'] = np.sum(dZ, axis=1, keepdims=True)
